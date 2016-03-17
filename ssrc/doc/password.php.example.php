866afb5a292d5d1c95f1eaabb709693361d7d573
Snowstar Cyan <997596@Gmail.com>
2016-03-17 22:31:04 +0800

    pw_example

---
+++ ssrc/doc/password.php.example.php
@@ -0,0 +1,13 @@
+<?php
+// SSRC数据库 读写权限
+define('DB_NAME'    , '//');
+define('DB_CHARSET' , '//');
+define('DB_HOST'    , '//');
+define('DB_USERNAME', '//');
+define('DB_PASSWORD', '//');
+// Discuz 论坛，只读权限
+define('BBS_DB_NAME'    , '//');
+define('BBS_DB_CHARSET' , '//');
+define('BBS_DB_HOST'    , '//');
+define('BBS_DB_USERNAME', '//');
+define('BBS_DB_PASSWORD', '//');
