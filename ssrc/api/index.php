31b19a4567435160d296c88ddaeedb1c5f817bdb
Snowstar Cyan <997596@Gmail.com>
2016-11-23 20:11:47 +0800

    done: 1,2

--- ssrc/api/index.php
+++ ssrc/api/index.php
@@ -2,12 +2,9 @@
 
 require_once('../cres.php');
 
-$query = DStr($_GET['q'], 'nothing');
-switch ($query) {
-case 'lsres':
-    $result = CRes::Q('SELECT * FROM res', '检索资源');
-    //$json = array('status' => 'fail');
-    
+function jsonLsRes(){
+    $result = CRes::Q('SELECT id,t_fileup,t_update,votereview,votecomment,count_download,status FROM res', '检索资源');
+
     $json_lsres = array();
     
     while($row = mysql_fetch_array($result)){
@@ -16,7 +13,7 @@ case 'lsres':
 
         if($cansee){
             $json_lsres[] = array(
-                'res'            => encodeCSID($row['id']),
+                'id'            => (int)$row['id'],
                 't_fileup'       => (int)$row['t_fileup'],
                 't_update'       => (int)$row['t_update'],
                 'votereview'     => (int)$row['votereview'],
@@ -25,21 +22,69 @@ case 'lsres':
                 );
         }
     }
-    //$json['status'] = 'success';
 
-	echo json_encode($json_lsres);
+    return json_encode($json_lsres);
+}
+
+function _jsonResInfo($where){
+
+    $result = CRes::Q('SELECT id,t_create,author_bbsid,author_name,name,content,b_gamebase,fromurl,e_type FROM res WHERE ' . $where,'获取资源详情' );
+
+    $json_ret = array();
+    while($row = mysql_fetch_array($result)){
+        $json_ret[] = array(
+            'id'          => encodeCSID($row['id']),
+            't_create'    => (int)$row['t_create'],
+            'author_bbsid'=> (int)$row['author_bbsid'],
+            'author_name' => $row['author_name'],
+            'name'        => $row['name'],
+            'content'     => $row['content'],
+            'b_gamebase'  => (int)$row['b_gamebase'],
+            'fromurl'     => $row['fromurl'],
+            'e_type'      => $row['e_type']
+            );
+    }
+
+    return json_encode($json_ret);
+}
+
+function jsonResInfo(){
+    $resIds_str = $_REQUEST['resids'];
+    $resIds_raw = (explode(",",$resIds_str));
+    $resIds_num = array_map("decodeCSID", $resIds_raw);
+    $resIds_query = '(' . implode(', ',$resIds_num) . ')';
+
+    return _jsonResInfo('id IN '. $resIds_query);
+}
+
+function jsonResInfoUpdate(){
+    $t_update = (int)$_REQUEST['t_update'];
+
+    return _jsonResInfo("t_update > $t_update");
+}
+
+//http://ssrc.snowstarcyan.com/ssrc/api/
+
+$query = DStr($_GET['q'], 'nothing');
+switch ($query) {
+
+case 'lsres':
+    echo jsonLsRes();
     break;
+    
+case 'resinfo':
+    echo jsonResInfo();
+    break;
+
+case 'resinfoupdate':
+    echo jsonResInfoUpdate();
+    break;
+
 case 'nothing':
-?>
-nothing
-<?php
+    echo "nothing";
+    break;
 
-break;
 default:
-
-?>
-unknown query: <?=$query?>
-<?php
-
-break;
+    echo "unknown query: $query";
+    break;
 }
